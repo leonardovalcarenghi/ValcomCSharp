ad13efa85e558d2f29f5d7fdfec7e42e20821829
Leonardo Valcarenghi <leonardo.valcarenghi@safeweb.com.br>
2020-09-15 16:48:39 -0300

    Conexão com Banco de Dados

---
+++ ValcomCSharp/DataBase/Attributes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valcom.DataBase
+{
+    /// <summary>
+    /// Nome da Coluna para Leitura
+    /// </summary>
+    public class ColumnNameAttribute : System.Attribute
+    {
+        public string ColumnName;
+        public ColumnNameAttribute(string value) { this.ColumnName = value; }
+    }
+}
