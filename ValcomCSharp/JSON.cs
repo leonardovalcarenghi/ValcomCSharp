26ff27f396487e917a2411da7cf867a59c6aa483
Leonardo Valcarenghi <leonardo.valcarenghi@safeweb.com.br>
2020-09-15 16:53:19 -0300

    JSON incluido

---
+++ ValcomCSharp/JSON.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace Valcom
+{
+    public class JSON
+    {
+        #region Serialize
+
+        /// <summary>
+        /// Serializar objeto (Converter objeto para JSON)
+        /// </summary>
+        /// <param name="value">Objeto</param>
+        /// <returns>JSON em string.</returns>
+        public static string Serialize(object value)
+        {
+            JavaScriptSerializer JSS = new JavaScriptSerializer();
+            JSS.MaxJsonLength = int.MaxValue;
+            string json = JSS.Serialize(value);
+            return json;
+        }
+
+        /// <summary>
+        /// Serializar objeto (Converter objeto para JSON)
+        /// </summary>
+        /// <param name="value">Objeto</param>
+        /// <param name="json">String para o retorno do JSON.</param>
+        public static void Serialize(object value, ref string json)
+        {
+            JavaScriptSerializer JSS = new JavaScriptSerializer();
+            JSS.MaxJsonLength = int.MaxValue;
+            json = JSS.Serialize(value);
+        }
+
+        #endregion
+
+        #region Deserialize
+
+        /// <summary>
+        /// Desserializar JSON (Converter JSON para Objeto)
+        /// </summary>
+        /// <param name="value">JSON em String.</param>
+
+        public static T Deserialize<T>(string value)
+        {
+            JavaScriptSerializer JSS = new JavaScriptSerializer();
+            JSS.MaxJsonLength = int.MaxValue;
+            object obj = JSS.Deserialize<T>(value);
+            return (T)obj;
+        }
+
+        /// <summary>
+        /// Desserializar JSON (Converter JSON para Objeto)
+        /// </summary>
+        /// <param name="value">JSON em String.</param>
+        /// <param name="obj">JSON em String.</param>
+        public static void Deserialize<T>(string value, ref T obj)
+        {
+            JavaScriptSerializer JSS = new JavaScriptSerializer();
+            JSS.MaxJsonLength = int.MaxValue;
+            obj = JSS.Deserialize<T>(value);
+        }
+
+        #endregion
+    }
+}
