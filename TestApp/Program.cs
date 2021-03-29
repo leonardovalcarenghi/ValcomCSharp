ca0143edc469b0a14236d6bb48bd000dca62f700
Leonardo Valcarenghi <leonardo.valcarenghi@safeweb.com.br>
2021-03-29 20:40:42 -0300

    projeto de teste

---
+++ TestApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Valcom.DataBase;
+
+namespace TestApp
+{
+    class Program
+    {
+
+        private static string ConnectionString = @"Data Source=CUSCO\equipeinovacao; Initial Catalog=sDoc_Desenvolvimento; User ID=sDoc_App; Password=Safeweb123; Application Name=sDoc_Desenvolvimento";
+
+
+        static void Main(string[] args)
+        {
+            UserDTO user = null;
+            string query = "SELECT * FROM [sdoc].[Users] WHERE CPF = @CPF";
+            Connection connection = new Connection(ConnectionString, query);
+            connection.AddParameter("CPF", "03659993000");
+            connection.Read(ref user, true);
+
+            string json = Valcom.JSON.Serialize(user);
+            Console.WriteLine(json);
+            Console.ReadKey();
+        }
+
+
+        public class UserDTO
+        {
+            [ColumnName("UserID")]
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string CPF { get; set; }
+        }
+
+    }
+}
